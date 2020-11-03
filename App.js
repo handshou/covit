a098ff1998e92898d3647d944644f16033314f83
handshou <12982599+handshou@users.noreply.github.com>
2020-11-03 12:57:29 +0800

    Add details screen

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-const HomeScreen = () => {
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Details</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Details"
+        onPress={() => navigation.navigate("Details")}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,6 +35,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
